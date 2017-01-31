f0bb8e0ef01292b39f18274a3aa43a0b39504ae0
matt kersner <mattkersner@gmail.com>
2017-01-31 17:02:22 -0500

    commiting

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 const authHelpers = require('../auth/auth-helpers');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+/* GET users listing. */
 router.get('/', authHelpers.loginRequired, (req, res, next) => {
   res.render('user/index', {
-    user: req.user.dataValues
+    user: req.user.dataValues,
   });
 });
 
