a7391f33ccc7e5374f1f32b73a3f042693ed0335
matt kersner <mattkersner@gmail.com>
2017-02-01 14:37:43 -0500

    please work

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 
+//requiring routes and setting to variables
 const index = require('./routes/index');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
-var app = express();
+const app = express();
 
 //require dotenv and use config method
 require('dotenv').config();
@@ -49,7 +50,7 @@ app.use('/user', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
