31acc07dd2574458f8fe5897335c5ce37a5f131a
matt kersner <mattkersner@gmail.com>
2017-02-01 17:27:31 -0500

    favorites db

---
+++ routes/favorites.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var router = express.Router();
+var models = require('../db/models/index');
+
+
+// router.get('/', function(req, res) {
+//   models.Movies.findById({})
+// })
+
+router.post('/', function(req, res, next) {
+  models.Favorites.create({
+    user_id: req.body.id,
+    movie_id: req.body.id
+  }).then(function() {
+    res.redirect('../user');
+  });
+});
+
+module.exports = router;
