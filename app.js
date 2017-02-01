12dc1d46db388ce57dbebc6cc73617491b2f172e
Jerel Crespo <mrc220@live.com>
2017-02-01 12:52:56 -0500

    hard reset before merge

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var directors = require('./routes/directors');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const directors = require('./routes/directors');
 
-var app = express();
+const app = express();
 // override with POST having ?_method=PUT
 app.use(methodOverride('_method'));
 
@@ -41,7 +41,7 @@ app.use('/directors', directors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
